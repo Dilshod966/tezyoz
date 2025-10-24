d16dfb1bcc27bdfccac565d09a730d8085824be3
Dilshod966 <bohodirovdilshod3@gmail.com>
2025-10-24 16:44:21 +0500

    cholo2

--- index.js
+++ index.js
@@ -16,3 +16,23 @@ function surish(a) {
 }
 
 
+
+
+
+
+let body = document.body;
+let nav = document.getElementsByTagName('nav');
+let tugma = document.getElementsByClassName('tugma');
+let tanlovRejim = document.getElementById('tanlovRejim');
+
+tanlovRejim.addEventListener('change', ()=>{
+    if(tanlovRejim.value == "tun") {
+        body.style.backgroundColor = 'black';
+        body.style.color = 'white';
+    }
+    else {
+
+    }
+    
+})
+
