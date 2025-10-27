9b835e18bc66a453b86c9902113b436ceddac380
Dilshod966 <bohodirovdilshod3@gmail.com>
2025-10-27 17:54:15 +0500

    cholo]\

--- index.js
+++ index.js
@@ -32,6 +32,20 @@ let odiyuq = document.querySelector('main > div:nth-child(2)')
 let sign = document.getElementsByClassName('sign');
 let input = document.querySelectorAll('.sign input');
 let tugma3 = document.getElementsByClassName('tugma3');
+let paragrf = document.querySelectorAll('.sign p');
+let signpa = document.querySelectorAll('.sign p a');
+let flexchaSpan = document.querySelector('.flexcha span:nth-child(2)')
+let naticha = document.getElementsByClassName("natija");
+let natijaP = document.querySelector('.natija > div:nth-child(1) p');
+let natijaSpan = document.querySelectorAll('.natija span');
+let top3 =document.getElementsByClassName('top3')
+let navDiva =document.querySelectorAll('nav div a')
+let tugma4 = document.getElementsByClassName('tugma4');
+let vaqtSpan = document.querySelectorAll('.vaqt span');
+let suruvchi1 = document.getElementsByClassName('suruvchi1');
+let suruvchi2 = document.getElementsByClassName('suruvchi2');
+let kulrangSpan = document.querySelectorAll('.kulrang span');
+let trlar = document.querySelectorAll('.jadval table tr');
 function kun() {
     body.style.backgroundColor = `rgb(240, 240, 240)`;
     nav.style.borderBottom = '0.5px solid rgb(185, 185, 185);'
@@ -48,10 +62,66 @@ function kun() {
     siklBoxShadow(sign,'0 0 10px rgb(136, 136, 136)');
     siklBoxShadow(input,'0 1px 3px rgb(167, 167, 167)')
     siklcolor(tugma3,'white');
+    siklBackcolor(tugma3,'black');
+    siklhover(tugma3,'rgb(40, 40, 40)','black');
+    siklcolor(paragrf,'rgb(113, 113, 113)');
+    siklcolor(signpa,"black");
+    flexchaSpan.style.borderColor = 'rgb(104, 104, 104)';
+    flexchaSpan.style.color = 'rgb(66, 66, 66)';
+    siklBorder(naticha,"rgb(174, 174, 174)");
+    siklcolor(natijaP,'black');
+    siklcolor(natijaSpan,'rgb(95, 95, 95)');
+    siklBoxShadow(top3,'0 0 3px yellowgreen');
+    siklBackImage(top3,"linear-gradient(to right,rgb(247, 247, 152), white)")
+    siklcolor(navDiva,'black');
+    siklBackcolor(tugma4,'rgb(238, 238, 238)');
+    siklBorder(tugma4,'rgb(167, 167, 167)');
+    siklhover(tugma4,rgb(255, 255, 255),rgb(238, 238, 238));
+    siklBorder(vaqtSpan,'rgb(167, 167, 167)');
+    siklBackcolor(suruvchi1,'rgb(192, 192, 192)');
+    siklBackcolor(suruvchi2,'rgb(233, 233, 233)');
+    siklcolor(kulrangSpan,'rgb(147, 147, 147)');
+    siklBorderBottom(trlar,'rgb(192, 192, 192)');
+
 }
 
 
 function tun() {
+    body.style.backgroundColor = `rgba(10, 10, 10, 1)`;
+    nav.style.borderBottom = '0.5px solid rgb(185, 185, 185);'
+    siklcolor(tugma,'black');
+    siklhover(tugma,'white','rgba(0, 0, 0, 0)');
+    yoniq.style.backgroundColor = 'black';
+    main.style.backgroundColor = 'white';
+    siklBorder(Mainspan,'rgb(206, 206, 206)');
+    siklBoxShadow(tugma2,'0 0 4px rgb(171, 171, 171)');
+    siklhover(tugma2,'rgb(219, 219, 219)','rgba(0, 0, 0, 0)')
+    odiyuq.style.borderColor = 'rgb(198, 198, 198)';
+    odiyuq.style.color = 'rgb(198, 198, 198)';
+    siklBackcolor(sign,'white');
+    siklBoxShadow(sign,'0 0 10px rgb(136, 136, 136)');
+    siklBoxShadow(input,'0 1px 3px rgb(167, 167, 167)')
+    siklcolor(tugma3,'white');
+    siklBackcolor(tugma3,'black');
+    siklhover(tugma3,'rgb(40, 40, 40)','black');
+    siklcolor(paragrf,'rgb(113, 113, 113)');
+    siklcolor(signpa,"black");
+    flexchaSpan.style.borderColor = 'rgb(104, 104, 104)';
+    flexchaSpan.style.color = 'rgb(66, 66, 66)';
+    siklBorder(naticha,"rgb(174, 174, 174)");
+    siklcolor(natijaP,'black');
+    siklcolor(natijaSpan,'rgb(95, 95, 95)');
+    siklBoxShadow(top3,'0 0 3px yellowgreen');
+    siklBackImage(top3,"linear-gradient(to right,rgb(247, 247, 152), white)")
+    siklcolor(navDiva,'black');
+    siklBackcolor(tugma4,'rgb(238, 238, 238)');
+    siklBorder(tugma4,'rgb(167, 167, 167)');
+    siklhover(tugma4,rgb(255, 255, 255),rgb(238, 238, 238));
+    siklBorder(vaqtSpan,'rgb(167, 167, 167)');
+    siklBackcolor(suruvchi1,'rgb(192, 192, 192)');
+    siklBackcolor(suruvchi2,'rgb(233, 233, 233)');
+    siklcolor(kulrangSpan,'rgb(147, 147, 147)');
+    siklBorderBottom(trlar,'rgb(192, 192, 192)');
 
 }
 
@@ -70,10 +140,25 @@ function siklBackcolor(a,c) {
     }
 }
 
+function siklBackImage(a,c) {
+    let i = 0;
+    while(a[i] != undefined) {
+        a[i].style.backgroundImage = c;
+        i++;
+    }
+}
+
 function siklBorder(a,c) {
     let i = 0;
     while(a[i] != undefined) {
         a[i].style.borderColor = c;
+        i++;
+    }
+}
+function siklBorderBottom(a,c) {
+    let i = 0;
+    while(a[i] != undefined) {
+        a[i].style.borderBottomColor = c;
         i++;
     }
 }
